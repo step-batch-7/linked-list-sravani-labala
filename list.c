825c95c96a5eec84545137cbbdad5f5043ae9157
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-04-30 17:37:32 +0530

    added add_to_end function

--- list.c
+++ list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "list.h"
 List_ptr create_list(void)
 {
@@ -14,6 +15,21 @@ Node_ptr create_node(int value)
   new_node->value = value;
   new_node->next = NULL;
   return new_node;
+}
+Status add_to_end(List_ptr list, int value)
+{
+  Node_ptr new_node = create_node(value);
+  if (list->head == NULL)
+  {
+    list->head = new_node;
+  }
+  else
+  {
+    list->last->next = new_node;
+  }
+  list->last = new_node;
+  list->count++;
+  return Success;
 }
 void display(List_ptr list)
 {
--- numbers.c
+++ numbers.c
@@ -2,5 +2,9 @@
 #include "list.h"
 int main(void)
 {
+  List_ptr list = create_list();
+  add_to_end(list, 1);
+  add_to_end(list, 2);
+  display(list);
   return 0;
 }
