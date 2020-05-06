abc9e1fa057038e86f0143388a7b895ad6547c16
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-05-06 16:03:32 +0530

    modified unit tests for checking the list count as well as the element at that position

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@ void print_status(Status status)
   printf("%s\n", status ? "Success" : "Failure");
 }
 
-Status compare(List_ptr list, int position, int value)
+Status compare(List_ptr list, int position, int value, int count)
 {
   if (position > list->count - 1 || position < 0)
   {
@@ -17,16 +17,16 @@ Status compare(List_ptr list, int position, int value)
   {
     p_walk = p_walk->next;
   }
-  return p_walk->value == value;
+  return p_walk->value == value && list->count == count;
 }
 
 void test_add_to_end()
 {
   List_ptr list = create_list();
   add_to_end(list, 1);
-  print_status(compare(list, 0, 1));
+  print_status(compare(list, 0, 1, 1));
   add_to_end(list, 2);
-  print_status(compare(list, 1, 2));
+  print_status(compare(list, 1, 2, 2));
   destroy_list(list);
 }
 
@@ -34,9 +34,9 @@ void test_add_to_start()
 {
   List_ptr list = create_list();
   add_to_start(list, 1);
-  print_status(compare(list, 0, 1));
+  print_status(compare(list, 0, 1, 1));
   add_to_start(list, 2);
-  print_status(compare(list, 0, 2));
+  print_status(compare(list, 0, 2, 2));
   destroy_list(list);
 }
 
@@ -45,9 +45,9 @@ void test_add_unique()
   List_ptr list = create_list();
   add_to_end(list, 1);
   add_unique(list, 0);
-  print_status(compare(list, 1, 0));
+  print_status(compare(list, 1, 0, 2));
   add_unique(list, 1);
-  print_status(compare(list, 2, Failure));
+  print_status(compare(list, 2, Failure, 2));
   destroy_list(list);
 }
 
@@ -57,7 +57,7 @@ void test_clear_list()
   clear_list(list);
   add_to_end(list, 3);
   clear_list(list);
-  print_status(compare(list, 0, Failure));
+  print_status(compare(list, 0, Failure, 0));
   destroy_list(list);
 }
 
@@ -65,19 +65,19 @@ void test_insert_at()
 {
   List_ptr list = create_list();
   insert_at(list, 1, 0);
-  print_status(compare(list, 0, 1));
+  print_status(compare(list, 0, 1, 1));
   insert_at(list, 2, 0);
-  print_status(compare(list, 0, 2));
+  print_status(compare(list, 0, 2, 2));
   insert_at(list, 3, 1);
-  print_status(compare(list, 1, 3));
+  print_status(compare(list, 1, 3, 3));
   insert_at(list, 4, 2);
-  print_status(compare(list, 2, 4));
+  print_status(compare(list, 2, 4, 4));
   insert_at(list, 5, 4);
-  print_status(compare(list, 4, 5));
+  print_status(compare(list, 4, 5, 5));
   insert_at(list, 6, -1);
-  print_status(compare(list, -1, Failure));
+  print_status(compare(list, -1, Failure, 5));
   insert_at(list, 6, 10);
-  print_status(compare(list, 10, Failure));
+  print_status(compare(list, 10, Failure, 5));
   destroy_list(list);
 }
 
@@ -87,11 +87,11 @@ void test_remove_from_start()
   remove_from_start(list);
   add_to_end(list, 1);
   remove_from_start(list);
-  print_status(compare(list, 0, Failure));
+  print_status(compare(list, 0, Failure, 0));
   add_to_end(list, 1);
   add_to_end(list, 2);
   remove_from_start(list);
-  print_status(compare(list, 0, 2));
+  print_status(compare(list, 0, 2, 1));
   destroy_list(list);
 }
 
@@ -101,11 +101,11 @@ void test_remove_from_end()
   remove_from_end(list);
   add_to_end(list, 1);
   remove_from_end(list);
-  print_status(compare(list, 0, Failure));
+  print_status(compare(list, 0, Failure, 0));
   add_to_end(list, 1);
   add_to_end(list, 2);
   remove_from_end(list);
-  print_status(compare(list, 1, Failure));
+  print_status(compare(list, 1, Failure, 1));
   destroy_list(list);
 }
 
@@ -115,14 +115,14 @@ void test_remove_first_occurrence()
   remove_first_occurrence(list, 1);
   add_to_end(list, 1);
   remove_first_occurrence(list, 1);
-  print_status(compare(list, 0, Failure));
+  print_status(compare(list, 0, Failure, 0));
   add_to_end(list, 1);
   remove_first_occurrence(list, 2);
-  print_status(compare(list, 0, 1));
+  print_status(compare(list, 0, 1, 1));
   add_to_end(list, 1);
   remove_first_occurrence(list, 1);
-  print_status(compare(list, 0, 1));
-  print_status(compare(list, 1, Failure));
+  print_status(compare(list, 0, 1, 1));
+  print_status(compare(list, 1, Failure, 1));
   destroy_list(list);
 }
 
@@ -132,13 +132,13 @@ void test_remove_all_occurrences()
   remove_all_occurrences(list, 1);
   add_to_end(list, 1);
   remove_all_occurrences(list, 1);
-  print_status(compare(list, 0, Failure));
+  print_status(compare(list, 0, Failure, 0));
   add_to_end(list, 1);
   add_to_end(list, 2);
   add_to_end(list, 1);
   remove_all_occurrences(list, 1);
-  print_status(compare(list, 0, 2));
-  print_status(compare(list, 1, Failure));
+  print_status(compare(list, 0, 2, 1));
+  print_status(compare(list, 1, Failure, 1));
   destroy_list(list);
 }
 
@@ -148,22 +148,22 @@ void test_remove_at()
   remove_at(list, 0);
   add_to_end(list, 1);
   remove_at(list, 0);
-  print_status(compare(list, 0, Failure));
+  print_status(compare(list, 0, Failure, 0));
   add_to_end(list, 1);
   add_to_end(list, 2);
   add_to_end(list, 3);
   add_to_end(list, 4);
   add_to_end(list, 5);
   remove_at(list, 0);
-  print_status(compare(list, 0, 2));
+  print_status(compare(list, 0, 2, 4));
   remove_at(list, 1);
-  print_status(compare(list, 1, 4));
+  print_status(compare(list, 1, 4, 3));
   remove_at(list, 3);
-  print_status(compare(list, 2, 5));
+  print_status(compare(list, 2, 5, 3));
   remove_at(list, 2);
-  print_status(compare(list, 2, Failure));
+  print_status(compare(list, 2, Failure, 2));
   remove_at(list, -5);
-  print_status(compare(list, 0, 2));
+  print_status(compare(list, 0, 2, 2));
   destroy_list(list);
 }
 
